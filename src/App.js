00db984df1566d807d86d1afc0dc961decd6fdc3
csonfack <sonfackyann@gmail.com>
2025-09-25 08:26:07 +0100

    logo and desgin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+import ContactMe from "./components/ContactMe";
 
 function App() {
+  const handleFormSubmit = (formData) => {
+    console.log("Form submitted:", formData);
+    // You can use navigate hook inside ContactMe component instead
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/services" element={<Services />} />
+            <Route
+              path="/contact"
+              element={<ContactMe handleFormSubmit={handleFormSubmit} />}
+            />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
